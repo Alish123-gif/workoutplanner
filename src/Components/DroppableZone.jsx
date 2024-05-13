4f088d62fcbf047df4b9758fc5dbdb3293b56a1f
Ali Shibli <84719984+Alish123-gif@users.noreply.github.com>
2024-05-13 08:41:57 +0300

    Update DroppableZone.jsx

--- src/Components/DroppableZone.jsx
+++ src/Components/DroppableZone.jsx
@@ -23,8 +23,8 @@ export default function DroppableZone({ day, onDrop, exercises, updateSets, upda
     const dayStyle = isOpen ? 'text-center w-full ' : 'text-white font-semibold text-2xl rotate-90 origin-bottom mt-4 mr-4 ';
 
     return (
-        <div ref={drop} onClick={toggleOpen} className={`border-2 m-2 inline-block  ${containerStyle} ${backgroundColor}`} >
-            <div className={`${dayStyle}`} >{day}</div>
+        <div ref={drop} onClick={isOpen?null: toggleOpen} className={`border-2 m-2 inline-block  ${containerStyle} ${backgroundColor}`} >
+            <div className={`${dayStyle}`} onClick={isOpen? toggleOpen:null} >{day}</div>
             {isOpen && exercises.map(
                 exercise => (
                     <DraggableCard
